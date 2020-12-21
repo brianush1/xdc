88b6889dc3ca6d91ac793e47ba896e0dc404d832
brianush1 <brianush1@outlook.com>
2020-12-21 15:13:46 -0500

    move keywords and symbols onto a single line

--- source/xdc/parser.d
+++ source/xdc/parser.d
@@ -10,12 +10,7 @@ import std.utf : stride, count;
 import std.conv;
 
 immutable(string[]) SYMBOLS = [
-	"/", "/=", ".", "..", "...", "&", "&=", "&&", "|",
-	"|=", "||", "-", "-=", "--", "+", "+=", "++", "<",
-	"<=", "<<", "<<=", ">", ">=", ">>=", ">>>=", ">>",
-	">>>", "!", "!=", "(", ")", "[", "]", "{", "}", "?",
-	",", ";", ":", "$", "=", "==", "*", "*=", "%", "%=",
-	"^", "^=", "^^", "^^=", "~", "~=", "@", "=>", "#",
+	"/", "/=", ".", "..", "...", "&", "&=", "&&", "|", "|=", "||", "-", "-=", "--", "+", "+=", "++", "<", "<=", "<<", "<<=", ">", ">=", ">>=", ">>>=", ">>", ">>>", "!", "!=", "(", ")", "[", "]", "{", "}", "?", ",", ";", ":", "$", "=", "==", "*", "*=", "%", "%=", "^", "^=", "^^", "^^=", "~", "~=", "@", "=>", "#",
 ].sort!((a, b) => b.count < a.count).array;
 
 static foreach (i, symbol; SYMBOLS) {
@@ -25,23 +20,7 @@ static foreach (i, symbol; SYMBOLS) {
 }
 
 immutable(string[]) KEYWORDS = [
-	"__FILE__", "__FILE_FULL_PATH__", "__FUNCTION__", "__gshared",
-	"__LINE__", "__MODULE__", "__parameters", "__PRETTY_FUNCTION__",
-	"__traits", "__vector", "abstract", "alias", "align", "asm",
-	"assert", "auto", "body", "bool", "break", "byte", "case", "cast",
-	"catch", "cdouble", "cent", "cfloat", "char", "class", "const",
-	"continue", "creal", "dchar", "debug", "default", "delegate",
-	"delete", "deprecated", "do", "double", "else", "enum", "export",
-	"extern", "false", "final", "finally", "float", "for", "foreach",
-	"foreach_reverse", "function", "goto", "idouble", "if", "ifloat",
-	"immutable", "import", "in", "inout", "int", "interface", "invariant",
-	"ireal", "is", "lazy", "long", "macro", "mixin", "module", "new",
-	"nothrow", "null", "out", "override", "package", "pragma", "private",
-	"protected", "public", "pure", "real", "ref", "return", "scope",
-	"shared", "short", "static", "struct", "super", "switch",
-	"synchronized", "template", "this", "throw", "true", "try", "typeid",
-	"typeof", "ubyte", "ucent", "uint", "ulong", "union", "unittest",
-	"ushort", "version", "void", "wchar", "while", "with",
+	"__FILE__", "__FILE_FULL_PATH__", "__FUNCTION__", "__gshared", "__LINE__", "__MODULE__", "__parameters", "__PRETTY_FUNCTION__", "__traits", "__vector", "abstract", "alias", "align", "asm", "assert", "auto", "body", "bool", "break", "byte", "case", "cast", "catch", "cdouble", "cent", "cfloat", "char", "class", "const", "continue", "creal", "dchar", "debug", "default", "delegate", "delete", "deprecated", "do", "double", "else", "enum", "export", "extern", "false", "final", "finally", "float", "for", "foreach", "foreach_reverse", "function", "goto", "idouble", "if", "ifloat", "immutable", "import", "in", "inout", "int", "interface", "invariant", "ireal", "is", "lazy", "long", "macro", "mixin", "module", "new", "nothrow", "null", "out", "override", "package", "pragma", "private", "protected", "public", "pure", "real", "ref", "return", "scope", "shared", "short", "static", "struct", "super", "switch", "synchronized", "template", "this", "throw", "true", "try", "typeid", "typeof", "ubyte", "ucent", "uint", "ulong", "union", "unittest", "ushort", "version", "void", "wchar", "while", "with",
 ];
 
 struct Token {
